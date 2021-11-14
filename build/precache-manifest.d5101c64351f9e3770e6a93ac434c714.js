054350d11cd050c51cf48a0a9af29a8ad24a97ab
Rahul Verma <raj369verma@gmail.com>
2021-11-14 13:48:15 +0530

    final

---
+++ build/precache-manifest.d5101c64351f9e3770e6a93ac434c714.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80d2372fb0353a43be1b93e5f15b6bdf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0844c15d17fbf3236670",
+    "url": "/static/css/main.1a2e4dae.chunk.css"
+  },
+  {
+    "revision": "b3a8da769d861d017cd5",
+    "url": "/static/js/2.397e526e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.397e526e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0844c15d17fbf3236670",
+    "url": "/static/js/main.e371220a.chunk.js"
+  },
+  {
+    "revision": "082f217487edfc4022a0",
+    "url": "/static/js/runtime-main.20eb6afc.js"
+  }
+]);
